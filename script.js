e1c3e17475cd304a887f6c0c38ffac08bba0ec10
Kristyna Podana <kristyna.zam@gmail.com>
2023-12-09 00:06:31 +0100

    Zmena souboru

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ setInterval(() => {
         seconds++
     }
 
-    document.body.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds),padStart(2, "0")}`
+    document.body.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`
 }, 1000)
 
 /* 
